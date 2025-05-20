4173b7f0eef0eb483948317229c18be393ec7ab4
Varshit Dusad <dusadvarshit@users.noreply.github.com>
2025-05-20 07:12:32 +0530

    Added custom logger and a /get route to test the functioning of mlflow server and model loading

--- src/churn_detection/deployment/app.py
+++ src/churn_detection/deployment/app.py
@@ -1,22 +1,26 @@
 import pandas as pd
 from flask import Flask, jsonify, render_template, request
 
+from churn_detection.utils.logger import CustomLogger
 from churn_detection.utils.mlflow_utils import fetch_model
 
+logger = CustomLogger("Eval").get_logger()
+
 app = Flask(__name__)
-
-# Load the model from MLflow
-
-# mlflow.set_tracking_uri(MLFLOW_TRACKING_URL)
-# logged_model = 'runs:/5009d9dbd0d243e8ab66b5b4db2f0530/model'
-# model = mlflow.sklearn.load_model(logged_model)
-
 model = fetch_model("staging")
 
 
 @app.route("/")
 def home():
     return render_template("form.html")
+
+
+@app.route("/get", methods=["GET"])
+def get():
+    logger.info("Get page")
+    model = fetch_model("staging")
+    logger.info(f"Model Loaded! Type {type(model)}")
+    return "Connection Succeeded"
 
 
 @app.route("/predict", methods=["POST"])
@@ -26,11 +30,13 @@ def predict():
         data = request.get_json()
 
         # Convert JSON to DataFrame
-        print(data)
+        logger.info(data)
         df = pd.DataFrame([data])
 
         # Make predictions
+        logger.info("Before prediction")
         predictions = model.predict(df)
+        logger.info("After prediction")
 
         # Return predictions as JSON
         predictions = predictions.tolist()[0]
@@ -41,8 +47,9 @@ def predict():
         return jsonify({"predictions": "The customer will NOT Churn!"})
 
     except Exception as e:
+        logger.info(f"ERROR during prediction! {str(e)}")
         return jsonify({"error": str(e)}), 400
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
