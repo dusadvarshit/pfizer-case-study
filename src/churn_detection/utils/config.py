d1181dcde832df7bd037cf94d8bb5a3c31b0bb59
Varshit Dusad <dusadvarshit@users.noreply.github.com>
2025-05-21 15:01:55 +0530

    Updated hyper-parameters dict to access optuna specific hyper-params as well

--- src/churn_detection/utils/config.py
+++ src/churn_detection/utils/config.py
@@ -1,5 +1,6 @@
 import os
 
+import optuna
 from dotenv import load_dotenv
 from lightgbm import LGBMClassifier
 from sklearn.ensemble import (
@@ -26,7 +27,7 @@ MLFLOW_ROOT_FOLDER = "../../../mlflow"
 
 MLFLOW_ARTIFACTS = "../../../mlflow/artifacts"
 
-HYPERPARAMS_DICT = binary_classification_models_param_grid = {
+binary_classification_models_param_grid = {
     "LogisticRegression": {
         "model": LogisticRegression(),
         "param_grid": {
@@ -34,15 +35,6 @@ HYPERPARAMS_DICT = binary_classification_models_param_grid = {
             "model__penalty": ["l2"],
             "model__solver": ["lbfgs", "liblinear"],
             "model__max_iter": [10, 100, 1000],
-        },
-    },
-    "RandomForestClassifier": {
-        "model": RandomForestClassifier(),
-        "param_grid": {
-            "model__n_estimators": [100, 200],
-            "model__max_depth": [None, 5, 10],
-            "model__min_samples_split": [2, 5],
-            "model__min_samples_leaf": [1, 2],
         },
     },
     "GradientBoostingClassifier": {
@@ -112,6 +104,73 @@ HYPERPARAMS_DICT = binary_classification_models_param_grid = {
             "model__n_estimators": [100, 200],
             "model__learning_rate": [0.01, 0.1],
             "model__max_depth": [3, 5, 7],
+        },
+    },
+    "RandomForestClassifier": {
+        "model": RandomForestClassifier(),
+        "param_grid": {
+            "model__n_estimators": [100, 200],
+            "model__max_depth": [None, 5, 10],
+            "model__min_samples_split": [2, 5],
+            "model__min_samples_leaf": [1, 2],
+        },
+    },
+}
+
+binary_classification_models_param_grid_optuna = {
+    "LogisticRegression": {
+        "model": LogisticRegression(),
+        "param_grid": {
+            "model__C": optuna.distributions.FloatDistribution(0.01, 10.0, log=True),
+            "model__penalty": optuna.distributions.CategoricalDistribution(["l2"]),
+            "model__solver": optuna.distributions.CategoricalDistribution(["lbfgs", "liblinear"]),
+            "model__max_iter": optuna.distributions.IntDistribution(10, 1000, log=True),
+        },
+    },
+    "GradientBoostingClassifier": {
+        "model": GradientBoostingClassifier(),
+        "param_grid": {"model__n_estimators": optuna.distributions.IntDistribution(100, 200), "model__learning_rate": optuna.distributions.FloatDistribution(0.01, 0.2, log=True), "model__max_depth": optuna.distributions.IntDistribution(3, 5)},
+    },
+    "AdaBoostClassifier": {"model": AdaBoostClassifier(), "param_grid": {"model__n_estimators": optuna.distributions.IntDistribution(50, 100), "model__learning_rate": optuna.distributions.FloatDistribution(0.01, 1.0, log=True)}},
+    "BaggingClassifier": {"model": BaggingClassifier(), "param_grid": {"model__n_estimators": optuna.distributions.IntDistribution(10, 100), "model__max_samples": optuna.distributions.FloatDistribution(0.5, 1.0), "model__max_features": optuna.distributions.FloatDistribution(0.5, 1.0)}},
+    "SVC": {
+        "model": SVC(probability=True),  # Enable probability for ROC AUC
+        "param_grid": {"model__C": optuna.distributions.FloatDistribution(0.1, 10.0, log=True), "model__kernel": optuna.distributions.CategoricalDistribution(["linear", "rbf", "poly"]), "model__gamma": optuna.distributions.CategoricalDistribution(["scale", "auto"])},
+    },
+    "DecisionTreeClassifier": {
+        "model": DecisionTreeClassifier(),
+        "param_grid": {"model__max_depth": optuna.distributions.CategoricalDistribution([None, 5, 10]), "model__min_samples_split": optuna.distributions.IntDistribution(2, 5), "model__min_samples_leaf": optuna.distributions.IntDistribution(1, 2)},
+    },
+    "KNeighborsClassifier": {
+        "model": KNeighborsClassifier(),
+        "param_grid": {"model__n_neighbors": optuna.distributions.IntDistribution(3, 10), "model__weights": optuna.distributions.CategoricalDistribution(["uniform", "distance"]), "model__metric": optuna.distributions.CategoricalDistribution(["euclidean", "manhattan"])},
+    },
+    "GaussianNB": {
+        "model": GaussianNB(),
+        "param_grid": {},  # No hyperparameters to tune
+    },
+    "XGBClassifier": {
+        "model": XGBClassifier(use_label_encoder=False, eval_metric="logloss"),  # print(classification_report(y_test, y_predict))
+        "param_grid": {
+            "model__learning_rate": optuna.distributions.FloatDistribution(0.01, 0.1, log=True),
+            "model__n_estimators": optuna.distributions.IntDistribution(100, 500),
+            "model__max_depth": optuna.distributions.IntDistribution(3, 7),
+            "model__min_child_weight": optuna.distributions.IntDistribution(1, 5),
+            "model__subsample": optuna.distributions.FloatDistribution(0.6, 1.0),
+            "model__colsample_bytree": optuna.distributions.FloatDistribution(0.6, 1.0),
+            "model__gamma": optuna.distributions.FloatDistribution(0, 0.3),
+            "model__reg_alpha": optuna.distributions.FloatDistribution(0, 0.1),
+            "model__reg_lambda": optuna.distributions.FloatDistribution(1, 2),
+        },
+    },
+    "LGBMClassifier": {"model": LGBMClassifier(verbose=-1), "param_grid": {"model__n_estimators": optuna.distributions.IntDistribution(100, 200), "model__learning_rate": optuna.distributions.FloatDistribution(0.01, 0.1, log=True), "model__max_depth": optuna.distributions.IntDistribution(3, 7)}},
+    "RandomForestClassifier": {
+        "model": RandomForestClassifier(),
+        "param_grid": {
+            "model__n_estimators": optuna.distributions.IntDistribution(100, 200),
+            "model__max_depth": optuna.distributions.CategoricalDistribution([None, 5, 10]),
+            "model__min_samples_split": optuna.distributions.IntDistribution(2, 5),
+            "model__min_samples_leaf": optuna.distributions.IntDistribution(1, 2),
         },
     },
 }
